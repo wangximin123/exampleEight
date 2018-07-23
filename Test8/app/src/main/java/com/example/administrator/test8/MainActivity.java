1e6e389db6b29c357f371992228f35e367bef24c
wxm <1174634507@qq.com>
2018-07-23 10:50:40 +0800

    eight-three-one

--- Test8/app/src/main/java/com/example/administrator/test8/MainActivity.java
+++ Test8/app/src/main/java/com/example/administrator/test8/MainActivity.java
@@ -4,39 +4,67 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.LongDef;
+import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView;
+    Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("result", MediaStore.EXTRA_OUTPUT);
+        imageView=findViewById(R.id.image);
 
     }
-    public void gotoSecond(View view){
-        Intent intent=new Intent(this,SecondActivity.class);
-        PendingIntent pendingIntent=PendingIntent.getActivity(this,0,intent,0);
-        NotificationManager notificationManager= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        Notification notification=new NotificationCompat.Builder(this).setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("This is Title").setContentText("This is Text")
-                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
-                .setWhen(System.currentTimeMillis())
-                .setContentIntent(pendingIntent)
-                .setSound(Uri.fromFile(new File("/system/media/audio/ringtones/Playa.oog")))
-                .setVibrate(new long[]{0L,1000L,1000L,1000L})
-                .setStyle(new NotificationCompat.BigTextStyle().bigText("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
-                .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(BitmapFactory.decodeResource(getResources(),R.drawable.aa)))
-                .setPriority(NotificationCompat.PRIORITY_MAX)
-                .build();
-        notificationManager.notify(1,notification);
+    public void photo(View view){
+        File outputImage=new File(getExternalCacheDir(),"output_image.jpg");
 
+        try {
+            if (outputImage.exists()){
+                outputImage.delete();
+            }
+            outputImage.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (Build.VERSION.SDK_INT>=24){
+            imageUri= FileProvider.getUriForFile(this,"com.example.administrator.test8.fileprovider",outputImage);
+        }else {
+            imageUri=Uri.fromFile(outputImage);
+        }
+        Intent intent=new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode==1){
+            try {
+                Bitmap bitmap=BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                Log.d("result",imageUri.toString());
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
