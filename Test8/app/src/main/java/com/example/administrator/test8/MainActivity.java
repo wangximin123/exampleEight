559e98424efe6c70927080694d4f1a64b5ffbbe7
wxm <1174634507@qq.com>
2018-07-23 08:57:20 +0800

    eight-two-one

--- Test8/app/src/main/java/com/example/administrator/test8/MainActivity.java
+++ Test8/app/src/main/java/com/example/administrator/test8/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.administrator.test8;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent=new Intent(this,SecondActivity.class);
+        PendingIntent pendingIntent=PendingIntent.getActivity(this,0,intent,0);
         NotificationManager notificationManager= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         Notification notification=new NotificationCompat.Builder(this).setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("This is Title").setContentInfo("This is information")
-                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher_background))
+                .setContentTitle("This is Title").setContentText("This is Text")
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
                 .setWhen(System.currentTimeMillis())
+                .setContentIntent(pendingIntent)
                 .build();
         notificationManager.notify(1,notification);
+    }
+    public void gotoSecond(View view){
+
+
     }
 }
